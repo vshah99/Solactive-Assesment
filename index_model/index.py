7a58954e17f79ea0e6b94f53602acc9c2329effb
JWuerzSolactive <jean-luc.wuerz@solactive.com>
2021-07-09 15:51:35 +0200

    Initial commit

---
+++ index_model/index.py
@@ -0,0 +1,15 @@
+import datetime as dt
+
+
+class IndexModel:
+    def __init__(self) -> None:
+        # To be implemented
+        pass
+
+    def calc_index_level(self, start_date: dt.date, end_date: dt.date) -> float:
+        # To be implemented
+        pass
+
+    def export_values(self, file_name: str):
+        # To be implemented
+        pass
